48d70a5673d12d9fff5a0f28c84aab8bd126d95d
Boone <boone@technoir.net>
2022-03-11 15:20:54 -0800

    unit tests

--- Samples/WildWorldImporters/Sales/Dal.SqlServer.Tests/Query.cs
+++ Samples/WildWorldImporters/Sales/Dal.SqlServer.Tests/Query.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace WildWorldImporters.Sales.Dal.SqlServer.Tests
@@ -25,6 +26,30 @@ namespace WildWorldImporters.Sales.Dal.SqlServer.Tests
             using var context = new SalesContext(Configuration.ConnectionString);
             var customer = context.Customers.OrderBy(c => c.Id).FirstOrDefault();
             Assert.NotNull(customer);
+        }
+
+        [Fact]
+        public void Take()
+        {
+            using var context = new SalesContext(Configuration.ConnectionString);
+            var customers = context.Customers.OrderBy(c => c.Id).Take(1).ToList();
+            Assert.True(customers.Count == 1, "customers.Count != 1");
+        }
+
+        [Fact]
+        public void Select()
+        {
+            using var context = new SalesContext(Configuration.ConnectionString);
+            var customers = context.Customers.OrderBy(c => c.Id).Select(c => c.PostalPostalCode).Take(1).ToList();
+            Assert.True(customers is List<string>, "customers is not List<string>");
+        }
+
+        [Fact]
+        public void CountWhere()
+        {
+            using var context = new SalesContext(Configuration.ConnectionString);
+            var postal_code = context.Customers.OrderBy(c => c.Id).Select(c => c.PostalPostalCode).First();
+            Assert.True(context.Customers.Count(c => c.PostalPostalCode == postal_code) > 0, "context.Customers.Count(c => c.PostalPostalCode == postal_code) <= 0");
         }
     }
 }
