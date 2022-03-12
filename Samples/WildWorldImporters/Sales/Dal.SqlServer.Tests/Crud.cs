423c865752e9e0f826f7473ae4284516ddd0d0ef
Boone <boone@technoir.net>
2022-03-12 13:56:24 -0800

    ScopeTransaction test Crud tests

---
+++ Samples/WildWorldImporters/Sales/Dal.SqlServer.Tests/Crud.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Transactions;
+using Xunit;
+
+namespace WildWorldImporters.Sales.Dal.SqlServer.Tests
+{
+    public class Crud
+    {
+        private static Color AddSetup()
+        {
+            using var context = new SalesContext(Configuration.ConnectionString);
+            var color         = new Color { LastEditedById = 1, Name = Guid.NewGuid().ToString().Substring(0, 20) };
+            context.Colors.Add(color);
+            context.SaveChanges();
+
+            return color;
+        }
+
+        [Fact]
+        public void Add()
+        {
+            using var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted });
+
+            var color_added = AddSetup();
+
+            using var context = new SalesContext(Configuration.ConnectionString);
+            var color         = context.Colors.SingleOrDefault(c => c.Id == color_added.Id);
+            Assert.NotNull(color);
+            Assert.Equal(color_added.LastEditedById, color.LastEditedById);
+            Assert.Equal(color_added.Name, color.Name);
+        }
+
+        private static Color DeleteSetup()
+        {
+            var color_added = AddSetup();
+
+            using var context = new SalesContext(Configuration.ConnectionString);
+            context.Colors.Remove(context.Colors.Single(c => c.Id == color_added.Id));
+            context.SaveChanges();
+
+            return color_added;
+        }
+
+        [Fact]
+        public void Delete()
+        {
+            using var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted });
+
+            var color_added = DeleteSetup();
+
+            using var context = new SalesContext(Configuration.ConnectionString);
+            Assert.True(context.Colors.All(c => c.Id != color_added.Id), "!context.Colors.All(c => c.Id != color_added.Id)");
+        }
+
+        private static Color ModifySetup()
+        {
+            var color_added = AddSetup();
+
+            using var context = new SalesContext(Configuration.ConnectionString);
+            var color         = context.Colors.Single(c => c.Id == color_added.Id);
+            color.Name        = new string(color_added.Name.Reverse().ToArray());
+            context.SaveChanges();
+
+            return color;
+        }
+
+        [Fact]
+        public void Modify()
+        {
+            using var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted });
+
+            var color_modified = ModifySetup();
+
+            using var context = new SalesContext(Configuration.ConnectionString);
+            var color         = context.Colors.SingleOrDefault(c => c.Id == color_modified.Id);
+            Assert.NotNull(color);
+            Assert.Equal(color_modified.LastEditedById, color.LastEditedById);
+            Assert.Equal(color_modified.Name, color.Name);
+        }
+    }
+}
--- Samples/WildWorldImporters/Sales/Dal.SqlServer.Tests/Transaction.cs
+++ Samples/WildWorldImporters/Sales/Dal.SqlServer.Tests/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Transactions;
 using Xunit;
 
 namespace WildWorldImporters.Sales.Dal.SqlServer.Tests
@@ -13,13 +14,29 @@ namespace WildWorldImporters.Sales.Dal.SqlServer.Tests
         }
 
         [Fact]
-        public void Rollback()
+        public void ConnectionRollback()
         {
             var count = RollbackSetup();
 
-            using var context     = new SalesContext(Configuration.ConnectionString);
-            
+            using var context = new SalesContext(Configuration.ConnectionString);
+
             using (var transaction = context.Database.BeginTransaction())
+            {
+                context.Colors.Add(new Color { LastEditedById = 1, Name = Guid.NewGuid().ToString().Substring(0, 20) });
+                context.SaveChanges();
+            }
+
+            Assert.Equal(count, context.Colors.Count());
+        }
+
+        [Fact]
+        public void ScopeRollback()
+        {
+            var count = RollbackSetup();
+
+            using var context = new SalesContext(Configuration.ConnectionString);
+
+            using (var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted }))
             {
                 context.Colors.Add(new Color { LastEditedById = 1, Name = Guid.NewGuid().ToString().Substring(0, 20) });
                 context.SaveChanges();
