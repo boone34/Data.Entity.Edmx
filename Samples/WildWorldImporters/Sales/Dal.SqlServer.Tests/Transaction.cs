ef3530d101de39249e13c8430afa844992e0dc7a
Boone <boone@technoir.net>
2022-03-11 17:08:13 -0800

    Transaction test

---
+++ Samples/WildWorldImporters/Sales/Dal.SqlServer.Tests/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace WildWorldImporters.Sales.Dal.SqlServer.Tests
+{
+    public class Transaction
+    {
+        private static int RollbackSetup()
+        {
+            using var context = new SalesContext(Configuration.ConnectionString);
+            return context.Colors.Count();
+        }
+
+        [Fact]
+        public void Rollback()
+        {
+            var count = RollbackSetup();
+
+            using var context     = new SalesContext(Configuration.ConnectionString);
+            
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                context.Colors.Add(new Color { LastEditedById = 1, Name = Guid.NewGuid().ToString().Substring(0, 20) });
+                context.SaveChanges();
+            }
+
+            Assert.Equal(count, context.Colors.Count());
+        }
+    }
+}
